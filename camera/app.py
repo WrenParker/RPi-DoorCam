0559f9e024df9ff89ec40148c41bf560ea866373
Wren Parker <wrenparker@gmail.com>
2019-03-08 10:21:35 -0500

    first test of lcd

--- camera/app.py
+++ camera/app.py
@@ -1,15 +1,19 @@
 from SendEmail import SendEmail
 from DetectMotion import DetectMotion
+from display import Display
 import time
 
 def run():
     email = SendEmail()
     motionDetect = DetectMotion()
+    display = Display()
     while True:
         motion = motionDetect.observeRoom()
         if motion == 1:
             email.sendEmail()
+            display.sayHello()
             time.sleep(10*60)
             motion = 0
         else:
             motion = 0
+            display.standBy()
---
+++ camera/display.py
@@ -0,0 +1,29 @@
+import Adafruit_CharLCD as LCD
+
+class Display:
+
+    def __init__(self):
+        self.rs = 25
+        self.en = 24
+        self.d4 = 23
+        self.d5 = 17
+        self.d6 = 18
+        self.d7 = 22
+        self.backLight = 4
+        self.columns = 16
+        self.rows = 2
+
+    def __main__(self):
+        pass
+    def setUp(self):
+        self.lcd = LCD.Adafruit_CharLCD(self.rs, self.en, self.d4, self.d5, self.d6, self.d7, self.backLight, self.columns, self.rows)
+        lcd.home()
+
+    def sayHello(self):
+        lcd.home()
+        lcd.message("ENEMY SPOTTED\n")
+        lcd.message("Hello There!")
+
+    def standBy(self):
+        lcd.home()
+        lcd.message("All Clear!")
